cbc7277cc7e8a8e4fc27e575818edb8cc34709e8
Pavlo <real.floordiv@gmail.com>
2024-06-19 23:56:47 +0200

    drafts of request unit tests

---
+++ tests/test_request.c
@@ -0,0 +1,19 @@
+//
+// Created by pavlo on 19.06.24.
+//
+
+#include "net/http/request.h"
+#include "unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_comparators(void) {
+
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_comparators);
+    return UNITY_END();
+}
