90c6b8452a663e15006973cb9a09e4fddfaa2ea4
Pavlo <real.floordiv@gmail.com>
2024-06-27 23:17:23 +0200

    improved tests. Now testing returned status, too

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -31,32 +31,40 @@ static http_parser_t init_parser(void) {
 void test_no_headers(void) {
     http_parser_t parser = init_parser();
     char* sample = "GET / HTTP/1.1\r\n\r\n";
-    http_parse(&parser, (byte_t*)sample, strlen(sample));
-    printf("method: %d\n", parser.request->method.method);
+    http_parser_status_t status = http_parse(&parser, (byte_t*)sample, strlen(sample));
+    TEST_ASSERT(status.completed);
+    char* msg; asprintf(&msg, "parser returned error: %" PRIu16, status.error);
+    TEST_ASSERT_MESSAGE(status.error == 0, msg);
+    TEST_ASSERT(status.extra_size == 0);
+
     TEST_ASSERT(parser.request->method.method == GET);
-    TEST_ASSERT(slice_cmp(parser.request->method.repr, new_slice("GET", 3)));
-    TEST_ASSERT(slice_cmp(parser.request->path, new_slice("/", 1)));
-    TEST_ASSERT(slice_cmp(parser.request->protocol, new_slice("HTTP/1.1", 8)));
+    TEST_ASSERT(slice_cmp(parser.request->method.repr, slice_new("GET", 3)));
+    TEST_ASSERT(slice_cmp(parser.request->path, slice_new("/", 1)));
+    TEST_ASSERT(slice_cmp(parser.request->protocol, slice_new("HTTP/1.1", 8)));
     TEST_ASSERT(keyval_len(&parser.request->headers) == 0);
 }
 
 void test_with_headers(void) {
     http_parser_t parser = init_parser();
     char* sample = "GET / HTTP/1.1\r\nHello: World\r\nContent-Length: 13\r\n\r\n";
-    http_parse(&parser, (byte_t*)sample, (ssize_t)strlen(sample));
-    printf("method: %d\n", parser.request->method.method);
+    http_parser_status_t status = http_parse(&parser, (byte_t*)sample, (ssize_t)strlen(sample));
+    TEST_ASSERT(status.completed);
+    char* msg; asprintf(&msg, "parser returned error: %" PRIu16, status.error);
+    TEST_ASSERT_MESSAGE(status.error == 0, msg);
+    TEST_ASSERT(status.extra_size == 0);
+
     TEST_ASSERT(parser.request->method.method == GET);
-    TEST_ASSERT(slice_cmp(parser.request->method.repr, new_slice("GET", 3)));
-    TEST_ASSERT(slice_cmp(parser.request->path, new_slice("/", 1)));
-    TEST_ASSERT(slice_cmp(parser.request->protocol, new_slice("HTTP/1.1", 8)));
+    TEST_ASSERT(slice_cmp(parser.request->method.repr, slice_new("GET", 3)));
+    TEST_ASSERT(slice_cmp(parser.request->path, slice_new("/", 1)));
+    TEST_ASSERT(slice_cmp(parser.request->protocol, slice_new("HTTP/1.1", 8)));
     TEST_ASSERT(keyval_len(&parser.request->headers) == 2);
     TEST_ASSERT(slice_cmp(
-        keyval_get(&parser.request->headers, new_slice("hello", 5))->value,
-        new_slice("World", 5)
+        keyval_get(&parser.request->headers, slice_new("hello", 5))->value,
+        slice_new("World", 5)
     ));
     TEST_ASSERT(slice_cmp(
-        keyval_get(&parser.request->headers, new_slice("content-length", 14))->value,
-        new_slice("13", 2)
+        keyval_get(&parser.request->headers, slice_new("content-length", 14))->value,
+        slice_new("13", 2)
     ));
     TEST_ASSERT(parser.request->content_length == 13);
 }
