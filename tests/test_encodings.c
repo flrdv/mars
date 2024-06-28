f931eaed50f89c9db86aff76dc4f46268e0130ee
Pavlo <real.floordiv@gmail.com>
2024-06-28 21:05:55 +0200

    setting up for encodings tests

---
+++ tests/test_encodings.c
@@ -0,0 +1,19 @@
+//
+// Created by pavlo on 28.06.24.
+//
+
+#include "net/http/encodings/plain.h"
+#include "unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_plain(void) {
+
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_plain);
+    return UNITY_END();
+}
