a5e6adf918b5e3687a8fd455fbb59c0ed7e8988c
Pavlo <real.floordiv@gmail.com>
2024-06-30 00:33:33 +0200

    fixed plain body reader: now it says when it's done

--- src/net/http/encodings/plain.c
+++ src/net/http/encodings/plain.c
@@ -2,19 +2,20 @@
 // Created by pavlo on 28.06.24.
 //
 
+#include <stdio.h>
 #include "plain.h"
 
-http_encoding_plain http_encoding_plain_new(void) {
+http_encoding_plain http_encoding_plain_new(const size_t content_length) {
     return (http_encoding_plain) {
-        .content_length = 0
+        .content_length = content_length
     };
 }
 
 http_encode_status http_encoding_plain_read(http_encoding_plain* self, const net_client* client) {
-    if (self->content_length == 0) return HTTP_ENCODE_STATUS_OK(SLICE_NULL);
+    if (self->content_length == 0) return HTTP_ENCODE_STATUS_DONE(SLICE_NULL);
 
-    net_status status = client->read(client->self);
-    if (!status.ok) return HTTP_ENCODE_STATUS_ERR(HTTP_ENCODE_ERR_READ);
+    const net_status status = client->read(client->self);
+    if (status.errno) return HTTP_ENCODE_STATUS_ERR(HTTP_ENCODE_ERR_READ);
 
     slice_t data = status.data;
     if (data.len <= self->content_length) {
@@ -27,5 +28,5 @@ http_encode_status http_encoding_plain_read(http_encoding_plain* self, const net
     data.len = self->content_length; // truncate the source slice a bit to avoid creating a new one
     self->content_length = 0;
 
-    return HTTP_ENCODE_STATUS_OK(data);
+    return HTTP_ENCODE_STATUS_DONE(data);
 }
--- src/net/http/encodings/plain.h
+++ src/net/http/encodings/plain.h
@@ -13,7 +13,7 @@ typedef struct http_encoding_plain {
     size_t content_length;
 } http_encoding_plain;
 
-http_encoding_plain http_encoding_plain_new(void);
+http_encoding_plain http_encoding_plain_new(size_t);
 http_encode_status http_encoding_plain_read(http_encoding_plain* self, const net_client* client);
 
 #endif //PLAIN_H
