ad4f0c38c870615b4487a04c489050dffd2c2c5c
Pavlo <real.floordiv@gmail.com>
2024-07-02 12:54:50 +0200

    body drafts

--- src/net/http/body.c
+++ src/net/http/body.c
@@ -2,6 +2,30 @@
 // Created by pavlo on 28.06.24.
 //
 
+#include <assert.h>
 #include "body.h"
 
+http_body_t http_body_new(net_client_t* client) {
+    return (http_body_t) {
+        .client = client,
+        .encsc = 0,
+    };
+}
 
+
+
+http_body_read_t http_body_read(http_body_t* self) {
+    assert(self->encsc > 0);
+
+    net_status_t read = self->client->read(self->client->self);
+    if (read.errno != 0)
+        return (http_body_read_t) { .errno = read.errno };
+
+    for (size_t i = 0; i < self->encsc; i++) {
+
+    }
+}
+
+void http_body_reset(http_body_t* self, http_request_t* request) {
+    // TODO: initialize encodings
+}
--- src/net/http/body.h
+++ src/net/http/body.h
@@ -2,13 +2,15 @@
 // Created by pavlo on 28.06.24.
 //
 
-#ifndef BODY_H
-#define BODY_H
+#ifndef MARS_NET_HTTP_BODY_H
+#define MARS_NET_HTTP_BODY_H
 
 #include <stdbool.h>
+#include "request.h"
 #include "net/client.h"
+#include "encodings/encoding.h"
 
-enum http_transfer_encoding {
+enum te_token_t {
     Chunked,
     GZIP,
     ZSTD
@@ -16,15 +18,19 @@ enum http_transfer_encoding {
 
 #define MAX_TRANSFER_ENCODINGS 5
 
-typedef struct http_body_te_t {
-    enum http_transfer_encoding encs[MAX_TRANSFER_ENCODINGS];
-} http_body_te_t;
-
 typedef struct http_body_t {
-    http_body_te_t transfer_encodings;
-    net_client_t client;
+    net_client_t* client;
+    size_t encsc;
+    http_encoding_t encs[MAX_TRANSFER_ENCODINGS];
 } http_body_t;
 
-slice_t http_body_read(http_body_t* self);
+typedef struct http_body_read_t {
+    size_t errno;
+    slice_t result;
+} http_body_read_t;
 
-#endif //BODY_H
+http_body_t http_body_new(net_client_t* client);
+http_body_read_t http_body_read(http_body_t* self);
+void http_body_reset(http_body_t* self, http_request_t* request);
+
+#endif //MARS_NET_HTTP_BODY_H
